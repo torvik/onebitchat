6f10e528107dc60e95154faf31d59a3c773d2642
torvik <pless.victor@gmail.com>
2018-04-04 19:22:25 -0300

    first

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  include CanCan::ControllerAdditions
+
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+
+
+end
